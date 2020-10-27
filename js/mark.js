09dca81a5112e38c8df0e24faf6bae4507ab38bb
HelgaVern <70060644+HelgaVern@users.noreply.github.com>
2020-10-27 19:39:08 +0300

    JS Catalog

---
+++ js/mark.js
@@ -0,0 +1,27 @@
+const markLink = document.querySelector(".button-catalog-buy");
+const markPopup = document.querySelector(".modal-mark");
+const markClose = markPopup.querySelector(".modal-close");
+
+document.querySelectorAll('.button-catalog-buy').forEach (markLink => markLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    markPopup.classList.add("modal-show");
+}));
+
+markLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    markPopup.classList.add("modal-show");
+});
+
+markClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    markPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (markPopup.classList.contains("modal-show")) {
+            evt.preventDefault();
+            markPopup.classList.remove("modal-show");
+        }
+    }
+});
